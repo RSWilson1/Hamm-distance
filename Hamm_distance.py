7bf4edac7d653cf167681fc66b048ec12937ea7e
Robert <robert.s.wilson1996@gmail.com>
2021-10-25 16:50:50 +0100

    Fixed filemode and added example files

--- Hamm_distance.py
+++ Hamm_distance.py
@@ -1,43 +1,39 @@
 import math
-import numpy as np
-import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description="Write sequences as text input for the two sequences to compare.")
-parser.add_argument('--Sequence_1', '-s1', type=str, metavar='', required=True, help='Type in sequence - not FASTA just raw bases.' )
-parser.add_argument('--Sequence_2', '-s2', type=str, metavar='', required=True, help='Type in sequence - not FASTA just raw bases.' )
-parser.add_argument('FileMode', '-F', type=argparse.FileType, metavar='', required=False, help='If pro' )
-
-
-parser.add_argument('--textSequence_1', '-s1', type=argparse.FileType, metavar='', required=False, help='Text file with sequence - not FASTA just raw bases.' )
-parser.add_argument('--textSequence_2', '-s2', type=argparse.FileType, metavar='', required=False, help='Text file with sequence - not FASTA just raw bases.' )
+parser.add_argument('--Sequence_1', '-s1', type=str, 
+                    metavar='', required=True, 
+                    help='Type in sequence - not FASTA just raw bases.' )
+parser.add_argument('--Sequence_2', '-s2', type=str, 
+                    metavar='', required=True, 
+                    help='Type in sequence - not FASTA just raw bases.' )
+parser.add_argument('-F', '--FileMode', action='store_true', required=False, help='Set file mode.')
 args = parser.parse_args()
 
 def Hamming_calc(input1, input2):
-    Hammer = 0
+    Hamming_distance = 0
     loop = 0
     try:
         for i in input1:
             if i != input2[loop]:
-                Hammer += 1
+                Hamming_distance += 1
                 loop += 1
             else:
                 loop += 1
     except Exception as e:
         print("Error with input, try writing the sequence as raw bases e.g AACGAATT ")
 
-    else: print(f'The Hamming distance is {Hammer}')
+    else: print(f'The Hamming distance is {Hamming_distance}')
 
 if __name__ == '__main__':
     if args.FileMode:
         with open(args.Sequence_1) as f:
-            seq1 = f.readlines()
+            seq1 = f.read().replace('\n', '')
+            print(seq1)
         with open(args.Sequence_2) as f:
-            seq2 = f.readlines()
+            seq2 = f.read().replace('\n', '')
+            print(seq2)
         Hamming_calc(seq1, seq2)
     else:
-        Hamming_calc(args.Sequence_1, args.Sequence_2)     
-
-#GAGCCTACTAACGGGAT
-#CATCGTAATGACGGCCT
-#/home/rswilson1/anaconda3/bin/python Hamm_distance.py "AAA" "TTT"
+        Hamming_calc(args.Sequence_1, args.Sequence_2)
